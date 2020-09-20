089827a11c3f02fafdf6fd5f2b5fbccef14e5c6d
jakabij <jakabistvanj@gmail.com>
2020-09-20 16:27:36 +0200

    added Store class

--- Project/Store.java
+++ Project/Store.java
@@ -1,5 +1,39 @@
 package RecepeStoreJava.Project;
 
+import java.util.ArrayList;
+
 public class Store {
-    
+
+    public ArrayList<RecipeBook> listOfRecipeBooks;
+
+    public Store()
+    {
+        listOfRecipeBooks = new ArrayList<RecipeBook>();
+    }
+
+    public void addRecipeBook(RecipeBook recipeBook)
+    {
+        listOfRecipeBooks.add(recipeBook);
+    }
+
+    public boolean removeRecipeBook(String id)
+    {
+        for(var book : listOfRecipeBooks)
+        {
+            if(book.getId().equals(id))
+            {
+                listOfRecipeBooks.remove(book);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<RecipeBook> getListOfRecipeBooks() {
+        return listOfRecipeBooks;
+    }
+
+    public void setListOfRecipeBooks(ArrayList<RecipeBook> listOfRecipeBooks) {
+        this.listOfRecipeBooks = listOfRecipeBooks;
+    }
 }
